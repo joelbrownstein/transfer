357e56a4e522bdd3c05dc8a2d9b48c6e760455a7
Joel Brownstein <joelbrownstein@astro.utah.edu>
2026-05-29 21:52:21 -0600

    mirror

--- python/transfer/Mirror.py
+++ python/transfer/Mirror.py
@@ -53,22 +53,6 @@ class Mirror:
     def set_user(self):
         try: self.user = environ['TRANSFER_GLOBUS_USER']
         except Exception as e: self.user = None
-    
-    def set_endpoint_base_dir(self, target=None, hpss=None):
-        try: target['base_dir'] = self.scratch_dir if hpss else environ['%(endpoint)s_BASE_DIR' % target]
-        except Exception as e: target['base_dir'] = '%r' % e
-
-    def set_endpoint_base_dir_for_sdss5_collection(self, target=None, hpss=None):
-        try:
-            if hpss:
-                target['base_dir'] = self.scratch_dir
-                uufs_home_dir = "/uufs/chpc.utah.edu/common/home"
-                if target['base_dir'] and target['base_dir'].startswith(uufs_home_dir):
-                    target['base_dir'] = target['base_dir'][len(uufs_home_dir):]
-            else:
-                target['base_dir'] = environ['%(endpoint)s_BASE_DIR' % target]
-                if target['endpoint'] == "SAS": target['base_dir'] = "/%s" % basename(target['base_dir'])
-        except Exception as e: target['base_dir'] = '%r' % e
 
     def set_item(self):
         base_dir = self.sas_endpoint['base_dir'] if self.sas_endpoint else None
@@ -93,9 +77,6 @@ class Mirror:
 
     def set_options(self,label=None,sync=None,preserve_mtime=False,fail_on_quota_errors=False,verify=False,delete=False,encrypt=False):
         self.options = {}
-        self.options['sas'] = "%(id)s:%(base_dir)s" % self.sas_endpoint
-        self.options['target'] = "%(id)s:%(base_dir)s"
-        self.options['target'] %= self.sam_endpoint if self.sam_endpoint else self.hpss_endpoint if self.hpss_endpoint else ''
         self.options['label'] = label if label else self.label
         self.options['sync'] = sync if sync in self.sync else None
         self.options['preserve_mtime'] = preserve_mtime
@@ -114,36 +95,6 @@ class Mirror:
         if self.options['label']: mode.append("--label=%(label)s")
         self.options['mode'] = " ".join(mode) % self.options
     
-    def set_endpoint_target(self, target):
-        if target:
-            if target['id']:
-                command = "globus endpoint is-activated %(id)s" % target
-                self.process.run(command)
-                if not self.process.status:
-                    lines = [line for line in self.process.out.split("\n") if line]
-                    response = lines[0] if len(lines)==1 else None
-                    active_response = "%(id)s is activated" % target
-                    alternate_response = "%(id)s does not require activation" % target
-                    inactive_response = "The endpoint is not activated." % target
-                    target['status'] = "active" if active_response else "personal endpoint (activation not required)" if alternate_response else "inactive"
-                    target['active'] = response == active_response or alternate_response
-                elif self.process.status==1:
-                    target['status'] = 'inactive (status code %r)' % self.process.status
-                    target['active'] = False
-                    self.ready = False
-                    self.info_message(message = "%r" % self.process.out)
-                else:
-                    target['status'] = 'inactive (status code %r)' % self.process.status
-                    target['active'] = None
-                    self.ready = False
-                    self.info_message(message = "Endpoint Error status code %r (bad syntax)" % self.process.status)
-            else:
-                target['status'] = 'Endpoint ID?'
-                target['active'] = None
-        
-            if self.verbose:
-                self.info_message(message = "%(endpoint)s is %(status)s" % target)
-
     def set_active_user(self):
         if self.ready:
             self.globus_cli.set_whoami()
