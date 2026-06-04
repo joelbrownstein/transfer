10978edab59937f3a6fb057bc72ed2d57f6c6e98
Joel Brownstein <joelbrownstein@sdss.org>
2026-06-04 11:38:10 -0600

    mirror

--- python/transfer/Transfer.py
+++ python/transfer/Transfer.py
@@ -268,10 +268,10 @@ class Transfer:
                 """mirror.execute_transfer()
                 if mirror.transfer:
                     mirror.wait()
-                    mirror.write_task_file()"""
+                    mirror.write_task_file()
                 else:
                     message = "ERROR! Globus failure to TRANSFER"
-                    self.ready = False
+                    self.ready = False"""
                 mirror.done()
             else:
                 message = "ERROR! Globus is not ready for MIRROR"
