ea70dc9c7309e0b84c3783c96964f329d71293e2
Joel Brownstein <joelbrownstein@sdss.org>
2026-06-12 17:29:17 -0600

    test

--- python/transfer/Mirror.py
+++ python/transfer/Mirror.py
@@ -21,6 +21,7 @@ class Mirror:
         self.mjd = options.mjd if options and hasattr(options, 'mjd') else mjd
         self.location = options.location if options else location
         self.release = self.location.split('/', 1) if self.location and '/' in self.location else None
+        print("RELEASE> %r" % self.release)
         self.save_manifest = options.save_manifest if options and 'save_manifest' in options else save_manifest
         self.manifest_only = options.manifest_only if options and 'manifest_only' in options else manifest_only
         self.dryrun = options.dryrun if options else dryrun
