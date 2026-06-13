fb1111377cb4cfafb8c1d7916f4ca6e2e9880888
Joel Brownstein <joelbrownstein@sdss.org>
2026-06-12 19:11:27 -0600

    test

--- python/transfer/Mirror.py
+++ python/transfer/Mirror.py
@@ -156,12 +156,14 @@ class Mirror:
 
             try:
                 directory, file = split(self.file['manifest'])
+                if self.verbose: print("MANIFEST> directory=%r file=%r" % (directory, file))
                 manifest_dir = join(directory, self.location)
                 source_manifest = join(manifest_dir, file)                
                 parts = source_manifest.split('%s/' % self.release, 1) if self.release else None
                 destination = join(self.release, parts[1]) if parts and len(parts) == 2 else None
                 destination_manifest = join(environ['TRANSFER_MIRROR_IPL_DIR'], destination )
                 self.manifest = {'source': source_manifest, 'destination': destination_manifest, 'location': location, 'locations': {'': getmtime(source_dir)}, 'symlinks': {}}
+                if self.verbose: print("MANIFEST> manifest=%r % self.manifest)
             except Exception as e:
                 self.error_message("Manifest aborted. %r" % e)
                 self.manifest = None
